c0e37f8aeb7934fa6f2633ff0c1fb00f51c6fad2
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-26 06:39:37 +0000

    simple etch a sketch project

---
+++ etch_a_sketch.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+
+
+tom = Turtle()
+screen = Screen()
+def move_forward() -> None:
+    """_Move turtle forward 10 paces_
+    """
+    tom.forward(10)
+def move_backward() -> None:
+    """_move turtle backward 10 paces_
+    """
+    tom.backward(10)
+def move_left()->None:
+    """_Turn turtle left 10 degrees_
+    """
+    to =tom.heading() + 10
+    tom.setheading(to)
+def move_right()->None:
+    """_Turn turtle right 10 degrees_
+    """
+    to = tom.heading() - 10
+    tom.setheading(to)
+
+def etch_a_sketch():
+    screen.listen()
+    screen.onkey(move_forward,"w")
+    screen.onkey(move_backward,"s")
+    screen.onkey(move_left,"a")
+    screen.onkey(move_right,"d")
+    screen.exitonclick()
+
+if __name__ == "__main__":
+    etch_a_sketch()
